b610ef0565b8da8ed5f9ccf7e6420182d72b5d84
dndbekfrl1 <dndbekfrl1@gmail.com>
2021-11-16 00:55:43 +0900

    Create Footer.tsx

---
+++ ridiclone/src/components/Footer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+import {theme} from "../theme/theme"
+
+const FooterBlock = styled.footer`  
+    width: 100%;
+    height: 3rem;
+    background: ${theme.colors.text};
+`
+
+const Footer = ()=>(
+    <FooterBlock>
+
+    </FooterBlock>
+)
+export default Footer
