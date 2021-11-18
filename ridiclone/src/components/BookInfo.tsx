1345108b220f39876fe5b79ac57315aa2378a0d5
dndbekfrl1 <dndbekfrl1@gmail.com>
2021-11-18 18:02:09 +0900

    Update BookInfo.tsx

--- ridiclone/src/components/BookInfo.tsx
+++ ridiclone/src/components/BookInfo.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components"
-import Stars from "../assets/img/stars.svg"
+import {ReactComponent as Stars} from "../assets/icon/stars.svg"
+import { theme } from "../theme/theme";
+import Button from "../components/Button"
 
 interface Representative{
     name:string,
@@ -30,15 +32,14 @@ interface StarRate{
 
 interface Book{
     id:number,
-    thumnail:string,
+    thumbnail:string,
     type?:string,
     category?:string[],
     title:string,
     author:Author,
     translator:Translator,
     publisher:string,
-    rating?:StarRate,
-    starRate?:any,
+    starRate:StarRate,
     rentalPrice?:number,
     rentalSalePercent?:number,
     buyPrice:number,
@@ -56,20 +57,49 @@ interface Book{
 
 const BookInfoBlock = styled.div`
     width:400px;
-    border:2px solid tomato;
     display:flex;
-
-    .thumnail{
-        img{
-            width: auto;
-            max-height: 8rem;
+    color:${theme.fontColor.gray};
+    font-size:${theme.fontSize.s}px;
+    .thumnail-wrapper{
+        display:flex;
+        flex-direction: column;
+        align-items: center;
+        margin-right: ${theme.spacing.l}rem;
+        .thumnail{
+            img{
+                width: auto;
+                max-height: 10rem;
+            }
+            margin-bottom: ${theme.spacing.s}rem;
         }
     }
     .bookinfo-wrapper{
+        .title{
+            margin: ${theme.spacing.s}rem 0 ;
+            color:${theme.fontColor.black};
+            font-size: ${theme.fontSize.l}px;
+        }
         .starRating{
             display:flex;
+            margin: ${theme.spacing.s}rem 0 ;
             .starRating_Icon{
-                background: src(${Stars});
+                position: relative;
+                .starRatin_fill{
+                    position: absolute;
+                    left:0;
+                    top:0;
+                }
+            }
+            .rate{
+                color:${theme.colors.star}
+            }
+            .rateNum{
+                color:${theme.fontColor.gray}
+            }
+        }
+        .metadata-wrapper{
+            div{
+                margin:${theme.spacing.xs}rem 0;
             }
         }
     }
@@ -77,29 +107,39 @@ const BookInfoBlock = styled.div`
 `
 
 
-const BookInfo = (book:Book)=>(
+const BookInfo = (book:Book)=>{ 
+    const starRatingFill = book.starRate.rate*10;
+    return(
     <BookInfoBlock>
         <div className="thumnail-wrapper">
             <div className="thumnail">
                 <img src={book.thumbnail}/>
             </div>
-            <button>좋아요</button>
+            <Button label="미리보기" size="medium"/>
         </div>
         <div className="bookinfo-wrapper">
-            <div className=""></div>
-            <div className="title">{book.title}</div>
+            <div className="">{book.type}</div>
+            <h3 className="title">{book.title}</h3>
             <div className="starRating">
                 <span className="starRating_Icon">
-                    <img src={Stars}/>
+                    <Stars className="starRatin_fill" height="10px" fill={theme.colors.star} viewBox={"0 0 "+ starRatingFill +" 10"} />
+                    <Stars fill={theme.colors.gray} height="10px" viewBox="0 0 50 10" />
                 </span>
-                <div>{book.starRate.rate}점</div>
-                <div>({book.starRate.rateNum})</div>
+                <div className="rate">{book.starRate.rate}점</div>
+                <div className="rateNum">({book.starRate.rateNum}명)</div>
             </div>
-            <div>
+            <div className="metadata-wrapper">
+                <div className="author">
+                    <b>{book.author.name}</b> 저
+                </div>
                 <div className="translator">
-            </div>
+                    <b>{book.translator.name}</b>
+                </div>
+                <div className="publisher">
+                    <b>{book.publisher}</b> 출판
+                </div>
             </div>
         </div>
     </BookInfoBlock>
-)
+)}
 export default BookInfo;
